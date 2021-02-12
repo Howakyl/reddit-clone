3373ee535474bdcaac24121448d9eb69854ff5d1
Howakyl96 <howakyl@gmail.com>
2021-02-11 16:23:30 -0800

    constants file for node.env prod vars

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello, world!')
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: 'reddit-clone',
+    type: 'postgresql',
+    debug: !__prod__,
+  });
+};
+
+
+// console.log('hello, world!')
+main();
