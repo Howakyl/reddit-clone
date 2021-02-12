58756bb10acf48e0daccdc0a407716d1e5a3a275
Howakyl96 <howakyl@gmail.com>
2021-02-11 18:30:24 -0800

    sanity check for express, connected

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   // the getMigrator function runs migrations before anything else executes.
+
+  const PORT = 4000;
+  const app = express();
+  app.get('/', (_,res) => {
+    res.send("hello")
+  })
+  app.listen(PORT, () => {
+    console.log(`server started on Port:${PORT}`)
+  })
 
   // const post = orm.em.create(Post, {title: 'my first post'});
   // await orm.em.persistAndFlush(post);
