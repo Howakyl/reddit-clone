2f98a61ccb7af55859f40ea9fd9c7d690afe21dd
Howakyl96 <howakyl@gmail.com>
2021-02-11 18:24:59 -0800

    dependencies graphql, apolloserver, type-graphql, express, apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,15 @@ import microConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  // the getMigrator function runs migrations before anything else executes.
 
-  const post = orm.em.create(Post, {title: 'my first post'});
-  await orm.em.persistAndFlush(post);
+  // const post = orm.em.create(Post, {title: 'my first post'});
+  // await orm.em.persistAndFlush(post);
   // this inserts the new post object into the database
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts)
 };
 
 main().catch(err => {
