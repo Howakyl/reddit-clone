fd3a44ffc38adb20dda323436a0d3ce04f73501a
Howakyl96 <howakyl@gmail.com>
2021-02-11 17:24:11 -0800

    added Post table to entities/ and onto main() function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
 const main = async () => {
   const orm = await MikroORM.init({
+    entities: [Post],
     dbName: 'reddit-clone',
     type: 'postgresql',
     debug: !__prod__,
