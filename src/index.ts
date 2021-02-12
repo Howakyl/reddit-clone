6923435fd65c0f3e6ac7fef69a14ab13b8e437d6
Howakyl96 <howakyl@gmail.com>
2021-02-11 17:45:32 -0800

    mikro-orm config, migrations object in MORM config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: 'reddit-clone',
-    type: 'postgresql',
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+  // this inserts the new post object into the database
 };
 
-
-// console.log('hello, world!')
-main();
+main().catch(err => {
+  console.log(err);
+});
