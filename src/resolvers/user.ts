8c9402fe61dd76231eca634c7c2b89101303b1fe
Howakyl96 <howakyl@gmail.com>
2021-02-18 14:24:45 -0800

    password hashing with argon2

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,7 @@
 import { User } from 'src/entities/User';
 import { MyContext } from 'src/types';
 import { Resolver, Mutation, Arg, InputType, Field, Ctx } from 'type-graphql';
+import argon2 from 'argon2';
 
 // this is an alternative to making multiple Arg() decorators
 // you can make a class, which is then passed into the Arg as it's type
@@ -24,7 +25,12 @@ export class UserResolver {
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
   ) {
-    const user = em.create(User, {username: options.username})
+    const hashedPassword = await argon2.hash(options.password)
+    // creates user with username, and hashed password from argon2
+    const user = em.create(User, {
+      username: options.username, 
+      password: hashedPassword
+    });
     await em.persistAndFlush(user);
     return user;
   }
